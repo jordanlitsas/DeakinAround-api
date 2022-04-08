00815108f015bdcbc2acf7ac42d83cb4500815f0
Jordan Litsas <jordan.litsas@gmail.com>
2022-04-08 20:28:09 +1000

    Welcome message for heroku deploy test

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.set('port', (process.env.PORT || 5000))
 const userRoute = require('./routes/users/userRoute');
 app.use('/api/user', userRoute);
 
+app.get('/', (req, res) => {
+  res.send("<h1>Welcome</h1>");
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
