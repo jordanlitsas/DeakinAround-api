2945ec1fbc8a988301bb24a2457381e211e9f7d1
Jordan Litsas <jordanlitsas@live.com.au>
2022-05-20 15:41:15 +1000

    unique page title req

--- models/schemas.js
+++ models/schemas.js
@@ -25,7 +25,8 @@ const user = new mongoose.Schema({
 const studentPage = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     description: {
         type: String,
